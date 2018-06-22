2cfe80795bd86874106e4f5c3cc48e653fb41200
jay-johnson <jay.p.h.johnson@gmail.com>
2018-06-21 17:24:16 -0700

    removed for flake8 on travis

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -156,6 +156,3 @@ texinfo_documents = [
      author, 'Spylunking', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-# -- Extension configuration -------------------------------------------------
